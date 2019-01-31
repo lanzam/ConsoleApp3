bc194040ae3269a7e655e12110ca9ffbd58add3f
Mariell <Mariell@DESKTOP-V0EECRJ>
2019-01-31 18:04:25 -0500

    Ver1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome! Please enter your name...");
+            
+            try
+            {
+                string input;
+                input = Console.ReadLine();
+                Console.WriteLine("Hello " + input);
+                Console.ReadKey(true);
+
+
+            }
+            catch
+            {
+                Console.WriteLine("Please enter valid characters...");
+                Console.WriteLine("To exit the program press any key...");
+            }
+           
         }
     }
 }
